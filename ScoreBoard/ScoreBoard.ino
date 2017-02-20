ea0f9bbf30fce00e29dfa16387e04d1886fba8fa
lolo <lolo.irk@gmail.com>
2017-02-20 17:29:31 +0800

    1

--- ScoreBoard/ScoreBoard.ino
+++ ScoreBoard/ScoreBoard.ino
@@ -4,12 +4,11 @@
  Author:	stoliarov.am2
 */
 
-// the setup function runs once when you press reset or power the board
 void setup() {
-
+	Serial.begin(9600);
 }
 
-// the loop function runs over and over again until power down or reset
 void loop() {
-  
+	Serial.write("1");
+	delay(500);
 }
