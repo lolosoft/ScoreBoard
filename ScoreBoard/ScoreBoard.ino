ee8b81632791eb0860317130324472af0b54ab31
lolo <lolo.irk@gmail.com>
2017-02-20 17:19:35 +0800

    Добавьте файлы проекта.

---
+++ ScoreBoard/ScoreBoard.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ScoreBoard.ino
+ Created:	20.02.2017 17:19:25
+ Author:	stoliarov.am2
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
